626f3857795746515c21d5dc386e7bf0ecd1ea55
Kent Rutan <gs1150e@icloud.com>
2022-04-16 16:46:37 -0500

    Tools: Update create_conda_env.sh
    Need a better way to manage environments but for now I'll update this
    because I need it.
    
    Pegged antlr4 version like setup.py, so changes to that will need to change
    here too.

--- tools/create_conda_env.sh
+++ tools/create_conda_env.sh
@@ -1,27 +1,30 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -eu
 if [ "$#" -eq 1 ]; then
     config=$1
 else
     echo "usage: $0 CONFIG"
-    echo "         possible configs (testing, enduser)"
+    echo "         possible configs (basic, notebook, develop, all)"
     exit 1
 fi
-
+basic="antlr4-python3-runtime==4.7 casadi sympy scipy jinja2 lxml matplotlib"
+develop="coverage pytest pytest-cov pytest-xdist pylint flake8 mypy tox"
+notebook="jupyterlab pydotplus control slycot"
+case "$config" in
+    basic)      packages="$basic";;
+    develop)    packages="$basic $develop";;
+    notebook)   packages="$basic $notebook";;
+    all)        packages="$basic $notebook $develop";;
+    *) echo "invalid config $config" >&2
+       exit 1;;
+esac
+set -x
 conda update -q conda
+# We're going to use pip for everything other than conda and python
+# so no real need for conda-forge here, but could be added if needed later
+# conda config --append channels conda-forge
 # Useful for debugging any issues with conda
 conda info -a
-conda config --add channels conda-forge
-conda create -n pymoca python=3.7 || echo "environment already created"
-conda install -n pymoca gcc jinja2 matplotlib numpy scipy sympy
+conda create -n pymoca python=3.9 || echo "environment already created"
 source activate pymoca
-# Note: casadi installed using pip since conda version currently
-# not found by setup.py
-pip install antlr4-python3-runtime casadi
-
-if [ "$config" == "testing" ]; then
-    conda install -n pymoca coverage coveralls
-elif [ "$config" == "eneduser" ]; then
-    conda install -n pymoca jupyter lapack pydotplus
-    pip install control slycot
-fi
+python -m pip install $packages

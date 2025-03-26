3b11da7e5739043d10bdc8df9316afbd19cac925
Kent Rutan <gs1150e@icloud.com>
2025-03-26 13:06:39 -0500

    parser/ast: Track replaceable
    Keep track of when both symbols and classes are replaceable so we can
    catch during instantiation if they are being redeclared when they are
    not replaceable.
    
    Properly track `replaceable` in the parser, including when it also
    means `redeclare`. Fix the parser to match the Modelica specification
    allowing one of three alternatives in a modification:
    
        1. `redeclare Real x`
        2. `redeclare replaceable Real x`
        3. `replaceable Real x`
    
    Alternative 3 means the same as alternative 2. All are redeclares of a
    symbol. We also fix the similar situation for class redeclares in
    modifications.
    
    Rename the existing Symbol `redeclare` attribute to `replaceable`
    since the former was not used and symbols/components can be
    replaceable without being redeclared. The parser now sets this when
    parsing a replaceable component.
    
    Add `replaceable` attribute to `ast.ShortClassDefinition` and
    `ast.ComponentClause` so we can fully track replaceable for both
    symbols and classes. We keep these two symmetric, though we will only
    currently use `ShortClassDefinition.replaceable` during instantiation
    of classes. The `replaceable` flag is propagated to the symbols in
    `ComponentClause` in the parser and then `Symbol.replaceable` is what
    will be used during instantiation for symbols.

--- src/pymoca/parser.py
+++ src/pymoca/parser.py
@@ -648,9 +648,11 @@ class ASTListener(ModelicaListener):
     def exitReplaceable_element(self, ctx: ModelicaParser.Replaceable_elementContext):
         if ctx.comp_elem is not None:
             self.ast[ctx] = self.ast[ctx.comp_elem]
+            for sym in self.ast[ctx].symbol_list:
+                sym.replaceable = True
         else:
             self.ast[ctx] = self.ast[ctx.class_elem]
-            self.ast[ctx].replaceable = True
+        self.ast[ctx].replaceable = True
 
     def enterComponent_clause(self, ctx: ModelicaParser.Component_clauseContext):
         prefixes = ctx.type_prefix().getText().split(" ")
@@ -786,8 +788,10 @@ class ASTListener(ModelicaListener):
     def exitElement_replaceable(self, ctx: ModelicaParser.Element_replaceableContext):
         if ctx.component_clause1() is not None:
             self.ast[ctx] = self.ast[ctx.component_clause1()]
+            self.ast[ctx].symbol_list[0].replaceable = True
         else:
             self.ast[ctx] = self.ast[ctx.short_class_definition()]
+        self.ast[ctx].replaceable = True
 
     def exitElement_modification_or_replaceable(
         self, ctx: ModelicaParser.Element_modification_or_replaceableContext
@@ -803,6 +807,8 @@ class ASTListener(ModelicaListener):
     def exitElement_redeclaration(self, ctx: ModelicaParser.Element_redeclarationContext):
         if ctx.component_clause1() is not None:
             self.ast[ctx] = self.ast[ctx.component_clause1()]
+        elif ctx.element_replaceable() is not None:
+            self.ast[ctx] = self.ast[ctx.element_replaceable()]
         else:
             self.ast[ctx] = self.ast[ctx.short_class_definition()]
         self.in_redeclaration = False
